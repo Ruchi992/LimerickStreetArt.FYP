4e756a77811edb369c0e023c06cf30f33d0fe96a
Ruchi Devi <ruchidevi1987@gmail.com>
2020-04-18 19:19:51 +0100

    new changes

---
+++ LimerickStreetArt.MySQL.UnitTests/Test.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LimerickStreetArt.MySQL.UnitTests
+{
+	class Test
+	{
+		public static void Main()
+		{
+			DatabaseClass databaseClass = new DatabaseClass();
+			databaseClass.ConnectionString = "";
+			UserAccountRepositoryClass userAccountRepositoryClass = new UserAccountRepositoryClass(databaseClass);
+
+			var userAccounts = userAccountRepositoryClass.SelectAll();
+
+			foreach (var userAccount in userAccounts)
+			{
+				Console.WriteLine(userAccount.Username);
+			}
+		}
+	}
+}
---
+++ LimerickStreetArt/Repository/StreetArtRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LimerickStreetArt.Repository
+{
+	public partial interface StreetArtRepository
+	{
+        StreetArt GetById(int StreetArtId);
+        List<StreetArt> SelectAll();
+        
+        void Delete(StreetArt streetart);
+        void Update(StreetArt streetart);
+        void Create(StreetArt streetart);
+    }
+}
