bc136d01ce403166f80c1cf6916e9d622d05df2f
Ruchi Devi <ruchidevi1987@gmail.com>
2020-05-07 17:11:59 +0100

    login page

---
+++ LimerickStreetArt.Web/Controllers/LoginUserAccount.cs
@@ -0,0 +1,35 @@
+﻿using LimerickStreetArt.MySQL;
+using LimerickStreetArt.Repository;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LimerickStreetArt.Web.Controllers
+{
+	public class LoginUserAccount
+	{
+		private readonly UserAccountRepository userAccountRepository;
+		private object userAccounts;
+
+		public LoginUserAccount(IConfiguration configuration)
+		{
+			var databaseClass = new DatabaseClass
+			{
+				ConnectionString = configuration.GetConnectionString("LocalDatabase"),
+			};
+			userAccountRepository = new UserAccountRepositoryClass(databaseClass);
+		}
+		public ActionResult Index()
+		{
+			return View(userAccounts);
+		}
+
+		private ActionResult View(Object userAccounts)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
