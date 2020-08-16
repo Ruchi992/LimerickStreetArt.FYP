8a46486855703a99b11b9cf810ea6533089d9f45
Ruchi Devi <ruchidevi1987@gmail.com>
2020-08-16 23:59:37 +0100

    Centralze Add Settings

---
+++ LimerickArtMap/LimerickArtMap/AppSettings.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LimerickStreetArt.MobileForms
+{
+	public static class AppUrls
+	{
+		public static int port = 5001;
+		public static String service = $"https://localhost:{port}/api/StreetArt/search/";
+		public static String login = $"{service}/login/";
+		public static String streatArt = $"{streatArt}/StreetArt/";
+		public static String streatArtSearch = $"{streatArtSearch}/search/";
+
+	}
+}
